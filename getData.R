945937516013b0addf91aeba81a12a4c8087ccf8
Chan-Yub Park <mrchypark@gmail.com>
2017-02-07 14:33:27 +0900

    Update getData.R

--- getData.R
+++ getData.R
@@ -21,7 +21,7 @@ links<-links[-c(1,2,3)]
     
     down<-strsplit(down,"'")[[1]]
     url<-paste0("http://gom.gomtv.com/main/index.html/?ch=subtitles&pt=down&intSeq=",down[2],"&capSeq=",down[4])
-    download.file(url,destfile = paste0("./data/",gsub("~","-",down[6])))
+    download.file(url,destfile = paste0("./data/",gsub("[^a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9.\\_ ]","",down[6])))
   }
   print(paste0(i," / ",maxPage))
 }
