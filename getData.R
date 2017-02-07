ded83a79cdf819069507b52f32e39baab072955e
mrchypark <mrchypark@gmail.com>
2017-02-07 09:38:52 +0000

    add prep.R

--- getData.R
+++ getData.R
@@ -9,7 +9,7 @@ tem<-read_html(url)
 maxPage<-tem %>% html_nodes("div.pagenavi div a") %>% html_attr("onclick")
 maxPage<-maxPage[length(maxPage)] %>% as.numeric
 
-for(i in 1:maxPage){
+for(i in 520:maxPage){
 url<-paste0("http://gom.gomtv.com/main/index.html?ch=subtitles&pt=l&menu=subtitles&lang=1&sWord=&page=",i)
 tem<-read_html(url)
 links<-tem %>% html_nodes("td.padd_left a") %>% html_attr("href")
@@ -20,12 +20,17 @@ links<-links[-c(1,2,3)]
     down<-tem %>% html_nodes("div.download a") %>% html_attr("onclick")
 
     if(identical(down,character(0))){next}
-    
+
     down<-strsplit(down,"'")[[1]]
     url<-paste0("http://gom.gomtv.com/main/index.html/?ch=subtitles&pt=down&intSeq=",down[2],"&capSeq=",down[4])
     title<-stri_trim_both(gsub("[^a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣0-9.\\_ ]","",down[6]))
     title<-gsub(" ","_",title)
-    download.file(url,destfile = paste0("./data/",title))
+    
+    var <- try(download.file(url,destfile = paste0("./data/",title)))
+    if(is(var, "try-error")){
+      Sys.sleep(1)
+      print("sleep 1 sec.")
+      next}
   }
   print(paste0(i," / ",maxPage))
 }
---
+++ prep.R
@@ -0,0 +1,3 @@
+dlist<-dir("./data")
+
+tem<-readLines(paste0("./data/",dlist[1]),encoding = "UTF-8")
