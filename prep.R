6b58b9ae4f7587b83e7d506235291d1e4a129489
Chan-Yub Park <mrchypark@gmail.com>
2017-02-09 19:11:55 +0900

    Update prep.R

--- prep.R
+++ prep.R
@@ -1,3 +1,40 @@
-dlist<-dir("./data")
+library(data.table)
+library(stringi)
 
-tem<-readLines(paste0("./data/",dlist[1]),encoding = "UTF-8")
+dl<-dir("./data",pattern = "smi",all.files = T)
+
+for(i in 1:length(dl)){
+
+  tem<-readLines(paste0("./data/",dl[i]),skipNul = T,warn = F)
+  if(length(tem)<50){next}
+  if(identical(tem,character(0))){next}
+  chk<-try(nchar(tem), silent = T)
+  if(class(chk)=="try-error"){next}
+  tem<-gsub("Sync","SYNC",tem)
+  tem<-gsub("sync","SYNC",tem)
+  tem<-tem[grep("<SYNC",tem)[1]:grep("<SYNC",tem)[length(grep("<SYNC",tem))]]
+  tem<-tem[-grep("<SYNC",tem)]
+  tem<-gsub("font","FONT",tem)
+  tem<-gsub("Font","FONT",tem)
+  if(!identical(grep("<FONT",tem),integer(0))){
+  tem<-tem[-grep("<FONT",tem)]}
+  tem<-gsub("<br>","",tem)
+  tem<-gsub("^- ","",tem)
+  tem<-gsub("^(file|gopher|news|nntp|telnet|https?|ftps?|sftp):\\/\\/.*","",tem)
+  tem<-tem[nchar(tem)>0]
+  tem<-unlist(strsplit(tem,"/"))
+  chk<-grep("^[ㄱ-ㅎ가-힣a-zA-Z0-9ㅏ-ㅣ\\.\\?!~ ]",tem)
+  if(!identical(chk,1:length(tem))){
+    tem<-tem[grepl("^[ㄱ-ㅎ가-힣a-zA-Z0-9ㅏ-ㅣ\\.\\?!~ ]",tem)]
+  }
+  tem<-gsub("[^ㄱ-ㅎ가-힣a-zA-Z0-9ㅏ-ㅣ\\.\\?!~ ]","",tem)
+  tem<-stri_trim_both(tem)
+  
+  title<-dl[i]
+  while(!identical(grep("\\.",strsplit(title,"")[[1]][1]),integer(0))){
+    title<-gsub("^\\.","",title)
+  }
+  title<-gsub("\\.smi","",title)
+  write.table(tem,paste0("./data_pre/",title,".txt"),row.names = F,col.names = F)
+  print(paste0(i, " / ",length(dl)))
+}
